8663d08174eab44fb6b2c2e825fd5cdec0b3a648
Tim Abell <tim@timwise.co.uk>
2024-12-17 21:04:41 +0000

    Call local netcat dummy server

--- HttpTriggerFunction.cs
+++ HttpTriggerFunction.cs
@@ -2,15 +2,18 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 public class HttpTriggerFunction
 {
     private readonly ILogger _logger;
+    private readonly HttpClient _httpClient;
 
-    public HttpTriggerFunction(ILoggerFactory loggerFactory)
+    public HttpTriggerFunction(ILoggerFactory loggerFactory, HttpClient httpClient)
     {
         _logger = loggerFactory.CreateLogger<HttpTriggerFunction>();
+        _httpClient = httpClient;
     }
 
     [Function("HttpTriggerFunction")]
@@ -20,7 +23,24 @@ public class HttpTriggerFunction
         _logger.LogInformation("C# HTTP trigger function processed a request.");
 
         var response = req.CreateResponse(HttpStatusCode.OK);
-        await response.WriteStringAsync("Hello, world!");
+
+        try
+        {
+            var result = await _httpClient.GetAsync("http://localhost:8899");
+            if (result.IsSuccessStatusCode)
+            {
+                await response.WriteStringAsync("Request to localhost:8899 was successful.");
+            }
+            else
+            {
+                await response.WriteStringAsync("Request to localhost:8899 failed.");
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError(ex, "Error calling localhost:8899");
+            await response.WriteStringAsync("Error calling localhost:8899");
+        }
 
         return response;
     }
